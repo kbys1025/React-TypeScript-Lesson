4c0972116ee134374f73e21b8251c4d808b3be20
kbys1025 <44672233+kbys1025@users.noreply.github.com>
2021-12-31 16:30:23 +0900

    PickをOmitに変更

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,9 +1,7 @@
 import { VFC } from "react";
 import { TodoType } from "./types/todo";
 
-export const Todo: VFC<Pick<TodoType, "userId" | "title" | "completed">> = (
-  props
-) => {
+export const Todo: VFC<Omit<TodoType, "id">> = (props) => {
   const { title, userId, completed = false } = props;
   const completeMark = completed ? "[完]" : "[未]";
   return <p>{`${completeMark}${title}(ユーザー: ${userId})`}</p>;
